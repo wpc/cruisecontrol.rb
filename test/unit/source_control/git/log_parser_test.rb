797826d59b4799524cd7a9207a95fda7d789c89c
wpc <alex.hal9000@gmail.com>
2008-10-15 21:37:25 +0800

    fix failure test for change sha1 revsion length from 5 to 7

--- test/unit/source_control/git/log_parser_test.rb
+++ test/unit/source_control/git/log_parser_test.rb
@@ -15,7 +15,7 @@ EOF
 
     def test_parse_should_work
       expected_revision = Git::Revision.new(
-                              'e51d6',
+                              'e51d66a',
                               'Alexey Verkhovsky <alexey.verkhovsky@gmail.com>',
                               Time.at(1209921867))
       revisions = Git::LogParser.new.parse(SIMPLE_LOG_ENTRY.split("\n"))
@@ -29,7 +29,7 @@ EOF
     def test_parse_line_should_recognize_commit_id_and_truncate_it_to_first_five_characters
       parser = Git::LogParser.new
       parser.send(:parse_line, "commit e51d66aa4f708fff1c87eb9afc9c48eaa8d5ffce")
-      assert_equal 'e51d6', parser.instance_variable_get(:@id)
+      assert_equal 'e51d66a', parser.instance_variable_get(:@id)
     end
 
     def test_parse_line_should_recognize_author
